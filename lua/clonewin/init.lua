e446cfb39e7d23c8782bb538813d81ce8a41bf97
Wansmer <wansmer@gmail.com>
2026-05-21 09:08:33 +0300

    fix: apply clone win on change active buffer

--- lua/clonewin/init.lua
+++ lua/clonewin/init.lua
@@ -11,7 +11,7 @@ function M.setup(opts)
   log.trace("Setup clonewin.nvim ==================================")
 
   vim.api.nvim_create_autocmd({ "BufEnter" }, {
-    callback = function(e)
+    callback = vim.schedule_wrap(function(e)
       local ft = vim.bo[e.buf].ft
       if vim.tbl_contains(cfg.config.ft, ft) then
         local win = vim.api.nvim_get_current_win()
@@ -20,12 +20,10 @@ function M.setup(opts)
         log.debug("%s event. Win: %s, Buf: %s, FT: %s", e.event, win, e.buf, ft)
 
         if win_cfg.relative == "" then
-          vim.schedule(function()
-            w.new(win, e.buf, cfg.config.override_ft[ft] or cfg.config)
-          end)
+          w.new(win, e.buf, cfg.config.override_ft[ft] or cfg.config)
         end
       end
-    end,
+    end),
   })
 end
 
